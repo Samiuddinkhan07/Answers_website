b9a18eb4864dddc3fbf3be64e82e550860a480c6
samiuddin00 <samiuddinkhan2000@gmail.com>
2023-10-02 22:04:12 +0530

    added new images to publication page

--- JS/script.js
+++ JS/script.js
@@ -5,10 +5,25 @@
             url:"templates/publications.htm",
             success:function(response){
                 $("#publication-page").html(response);
+                addImagesPub();
             }
         });
+        
     }
 
     $("#publicationget").click(function(){
         getPublicationPage();
     });
+
+
+    function addImagesPub(){
+        let _imagesArr = new Array(24).fill("publication_img");
+        for(let i = 0;i < _imagesArr.length;i++){
+            $("#pub-img").append(`
+            <li>
+            <img src="/Assets/Images/${_imagesArr[i]}-${i}.jpg" alt="${_imagesArr[i]}">
+            </li>
+            `);
+        }
+    }
+
